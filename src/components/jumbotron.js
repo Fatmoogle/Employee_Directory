5c3a71595e08b7917b7cc98fa9457e005e24e3b5
Alex Varela <alexvar93@gmail.com>
2020-11-03 12:49:34 -0500

    added jumbotron

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "../styles/jumbotron.css";
 
 function Jumbotron() {
     return (
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            </div>
+        <div className="jumbotron d-flex flex-column align-items-center p-4">
+            <h1 className="display-4">Employee Directory</h1>
+            <p className="lead">Search for employees by using the search bar provided.</p>
+            <hr />
         </div>
-    )
+    );
 }
 
 export default Jumbotron;
