e316da277a4675e1b28ccd703519aa45edbe51bf
Alex Varela <alexvar93@gmail.com>
2020-11-03 16:28:25 -0500

    added styling for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Jumbotron from "./components/jumbotron";
 import SearchBox from "./components/searchBox";
+import Table from "./components/table";
 
 function App() {
   return (
     <>
     <Jumbotron />
     <SearchBox />
+    <Table />
     </>
   );
 }
